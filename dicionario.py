9ee435fcf6f3670929aa983013e0051248fec3c8
Vitor Alves <162993371+VITORALVMR18@users.noreply.github.com>
2024-04-24 07:57:24 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,26 @@
+ListaNomes = []
+
+
+ListaNomes.append("Calleri")
+ListaNomes.append("Erick")
+ListaNomes.append("Ferreira")
+
+print(ListaNomes)
+print(ListaNomes(1))
+
+for nome in ListaNomes:
+    print(nome)
+
+PrimeiroDicionario = ()
+
+PrimeiroDicionario["nome"] = "Calleri"
+PrimeiroDicionario["tipo"] = "Jogador"
+PrimeiroDicionario["numero"] = 9
+print(PrimeiroDicionario)
+print(PrimeiroDicionario)
+
+
+# Anatomia
+# Lista[indice] = Valor 
+# dicionario[chave] = Valor 
+
