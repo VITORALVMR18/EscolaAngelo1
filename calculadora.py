9a4ad8a8d79e252f806f3fc61ca37bb0366ffdce
VITORALVMR18 <vitor1810176@gmail.com>
2024-05-29 08:08:12 -0300

    calcular teste

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+def calcular(x, y):
+    print(x + y)
+    print(x - y)
+    print(x * y)
+    print(x / y)
+    print(x % y)
+
+x = int(input('digite um numero'))
+y = int(input('digite um numero'))
+calcular(x, y)
